460e0471501a5b4b86ef33e5f02014dd6b80ea0c
hossein <namazian87@gmail.com>
2018-05-27 23:41:18 +0430

    student added

--- contracts/Course.sol
+++ contracts/Course.sol
@@ -78,38 +78,50 @@ contract Milestone is Materials {
         mapping(uint256 => uint8) quizShare;
     }
 
-    mileStone[] ALlMilestones;
+    mileStone[] AllMilestones;
     mapping(uint256 => uint256) MilestoneIndex;
 
     function createMilestone(uint256 _uniqueCode, string _name) public onlyOwner {
-        mileStone storage newMilestone = ALlMilestones[ALlMilestones.length];
+        mileStone storage newMilestone = AllMilestones[AllMilestones.length];
         newMilestone.uniqueCode = _uniqueCode;
         newMilestone.name = _name;
     }
 
     function addVideoToMilestone(uint256 _MilestoneUniqueCode, uint256 _VideoUniqueCode, uint8 _share) public onlyOwner {
         uint256 milestoneIndex = MilestoneIndex[_MilestoneUniqueCode];
-        mileStone storage milestone = ALlMilestones[milestoneIndex];
+        mileStone storage milestone = AllMilestones[milestoneIndex];
         milestone.videoIndex.push(getVideoIndex(_VideoUniqueCode));
         milestone.videoShare[_VideoUniqueCode] = _share;
     }
 
     function addCodeProjectToMilestone(uint256 _MilestoneUniqueCode, uint256 _CodeProjectUniqueCode, uint8 _share) public onlyOwner {
         uint256 milestoneIndex = MilestoneIndex[_MilestoneUniqueCode];
-        mileStone storage milestone = ALlMilestones[milestoneIndex];
+        mileStone storage milestone = AllMilestones[milestoneIndex];
         milestone.codeProjectIndex.push(getCodeProjectIndex(_CodeProjectUniqueCode));
         milestone.codeProjectShare[_CodeProjectUniqueCode] = _share;
     }
 
     function addQuizToMilestone(uint256 _MilestoneUniqueCode, uint256 _QuizUniqueCode, uint8 _share) public onlyOwner {
         uint256 milestoneIndex = MilestoneIndex[_MilestoneUniqueCode];
-        mileStone storage milestone = ALlMilestones[milestoneIndex];
+        mileStone storage milestone = AllMilestones[milestoneIndex];
         milestone.quizIndex.push(getQuizIndex(_QuizUniqueCode));
         milestone.quizShare[_QuizUniqueCode] = _share;
     }
 
     function getMilestoneIndex(uint256 _uniqueCode) internal view returns (uint256) {
         return MilestoneIndex[_uniqueCode];
+    }
+
+    function getProgressByVideo(uint256 _videoIndex, uint256 _milestoneIndex) internal view returns (uint8) {
+        return AllMilestones[_milestoneIndex].videoShare[_videoIndex];
+    }
+
+    function getProgressByCodeProject(uint256 _codeProjectIndex, uint256 _milestoneIndex) internal view returns (uint8) {
+        return AllMilestones[_milestoneIndex].codeProjectShare[_codeProjectIndex];
+    }
+
+    function getProgressByQuiz(uint256 _quizIndex, uint256 _milestoneIndex) internal view returns (uint8) {
+        return AllMilestones[_milestoneIndex].quizShare[_quizIndex];
     }
 }
 
@@ -119,7 +131,7 @@ contract Course is Milestone {
         string name;
 
         uint256[] MilestoneIndex;
-        mapping(uint256 => uint256) MilestoneShare;
+        mapping(uint256 => uint256) MilestoneReward;
 
         uint256 Reward;
     }
@@ -128,20 +140,25 @@ contract Course is Milestone {
     mapping(uint256 => uint256) CourseIndex;
 
     function createCourse(uint256 _uniqueCode, string _name, uint256 _reward) public onlyOwner {
-        course storage newCourse = AllCourses[_uniqueCode];
+        uint256 courseIndex = CourseIndex[_uniqueCode];
+        course storage newCourse = AllCourses[courseIndex];
         newCourse.uniqueCode = _uniqueCode;
         newCourse.name = _name;
         newCourse.Reward = _reward;
     }
 
-    function addMilestoneToCourse(uint256 _courseCode, uint256 _milestoneCode, uint8 _share) public onlyOwner {
+    function addMilestoneToCourse(uint256 _courseCode, uint256 _milestoneCode, uint8 _reward) public onlyOwner {
         uint256 courseIndex = CourseIndex[_courseCode];
         course storage _course = AllCourses[courseIndex];
         _course.MilestoneIndex.push(getMilestoneIndex(_milestoneCode));
-        _course.MilestoneShare[getMilestoneIndex(_milestoneCode)] = _share;
+        _course.MilestoneReward[getMilestoneIndex(_milestoneCode)] = _reward;
     }
 
     function getCourseIndex(uint256 _courseCode) internal view returns (uint256) {
         return CourseIndex[_courseCode];
+    }
+
+    function getMilestoneReward(uint256 _courseIndex, uint256 _milestoneIndex) internal view returns (uint256) {
+        return AllCourses[_courseIndex].MilestoneReward[_milestoneIndex];
     }
 }
---
+++ contracts/Student.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.4.23;
+
+import "./Course.sol";
+
+contract Student is Course {
+    struct student {
+        address Address;
+        string name;
+        uint256[] AllCourses;
+        mapping(uint256 => uint8) MilestoneProgress;
+        mapping(uint256 => bool) MilestonesDone;
+        mapping(uint256 => uint8) CoursesProgress;
+        mapping(uint256 => bool) CoursesDone;
+        mapping(uint256 => bool) VideosDone;
+        mapping(uint256 => bool) CodesDone;
+        mapping(uint256 => bool) QuizesDone;
+        uint256 Rewards;
+    }
+
+    student[] AllStudents;
+    mapping(address => uint256) StudentIndex; 
+
+    function RegisterNewStudent(address _student, string _name) public onlyOwner {
+        uint256 studentIndex = AllStudents.length;
+        student storage newStudent = AllStudents[studentIndex];
+        newStudent.Address = _student;
+        newStudent.name = _name;
+    }
+
+    function EnrollNewCourse(address _student, uint256 _courseCode) public onlyOwner {
+        uint256 courseIndex = getCourseIndex(_courseCode);
+        uint256 studentIndex = StudentIndex[_student];
+        student storage _Student = AllStudents[studentIndex];
+        _Student.AllCourses.push(courseIndex);
+    }
+
+    function videoCompleted(address _student, uint256 _courseCode, uint256 _milestoneCode, uint256 _videoCode) public onlyOwner {
+        uint256 studentIndex = StudentIndex[_student];
+        uint256 courseIndex = getCourseIndex(_courseCode);
+        uint256 milestoneIndex = getMilestoneIndex(_milestoneCode);
+        uint256 videoIndex = getVideoIndex(_videoCode);
+        uint8 progress = getProgressByVideo(milestoneIndex, videoIndex);
+        student storage _Student = AllStudents[studentIndex];
+        require(!_Student.VideosDone[videoIndex]);
+        _Student.VideosDone[videoIndex] = true;
+        _Student.MilestoneProgress[milestoneIndex] += progress;
+        if (_Student.MilestoneProgress[milestoneIndex] == 100) {
+            _Student.MilestonesDone[milestoneIndex] = true;
+            _Student.Rewards.add(getMilestoneReward(courseIndex, milestoneIndex));
+        }
+    }
+
+    function codeProjectCompleted(address _student, uint256 _courseCode, uint256 _milestoneCode, uint256 _codeProjectCode) public onlyOwner {
+        uint256 studentIndex = StudentIndex[_student];
+        uint256 courseIndex = getCourseIndex(_courseCode);
+        uint256 milestoneIndex = getMilestoneIndex(_milestoneCode);
+        uint256 codeIndex = getVideoIndex(_codeProjectCode);
+        uint8 progress = getProgressByVideo(milestoneIndex, codeIndex);
+        student storage _Student = AllStudents[studentIndex];
+        require(!_Student.CodesDone[codeIndex]);
+        _Student.CodesDone[codeIndex] = true;
+        _Student.MilestoneProgress[milestoneIndex] += progress;
+        if (_Student.MilestoneProgress[milestoneIndex] == 100) {
+            _Student.MilestonesDone[milestoneIndex] = true;
+            _Student.Rewards.add(getMilestoneReward(courseIndex, milestoneIndex));
+        }
+    }
+
+    function quizCompleted(address _student, uint256 _courseCode, uint256 _milestoneCode, uint256 _quizCode) public onlyOwner {
+        uint256 studentIndex = StudentIndex[_student];
+        uint256 courseIndex = getCourseIndex(_courseCode);
+        uint256 milestoneIndex = getMilestoneIndex(_milestoneCode);
+        uint256 quizIndex = getVideoIndex(_quizCode);
+        uint8 progress = getProgressByVideo(milestoneIndex, quizIndex);
+        student storage _Student = AllStudents[studentIndex];
+        require(!_Student.QuizesDone[quizIndex]);
+        _Student.QuizesDone[quizIndex] = true;
+        _Student.MilestoneProgress[milestoneIndex] += progress;
+        if (_Student.MilestoneProgress[milestoneIndex] == 100) {
+            _Student.MilestonesDone[milestoneIndex] = true;
+            _Student.Rewards.add(getMilestoneReward(courseIndex, milestoneIndex));
+        }
+    }
+}
